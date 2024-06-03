7eeb9fdf737419241cb1aee0dcddbca6b901a0d8
ios <87903312+iosrouter@users.noreply.github.com>
2024-06-02 19:22:05 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ mkdir -p ${PWD}/{usr/lib/,var/jb/usr/lib/}
 make clean package THEOS_PACKAGE_SCHEME=rootless
 find .theos/_/ -name "*.dylib" -exec cp -a {} ${PWD}/var/jb/usr/lib/ \;
 mkdir -p ${THEOS}/lib/iphone/rootless/
-cp -a ${PWD}/var/jb/usr/lib/libmryipc.dylib ${THEOS}/lib/iphone/rootless
-cp -a ${PWD}/include/ ${THEOS}/include/
 cp -a ${PWD}/MRYIPCCenter.h ${THEOS}/include/
 make clean package
 find .theos/_/ -name "*.dylib" -exec cp -a {} ${PWD}/usr/lib/ \;
 cp -a ${PWD}/usr/lib/libmryipc.dylib ${THEOS}/lib/
+cp -a ${PWD}/var/jb/usr/lib/libmryipc.dylib ${THEOS}/lib/iphone/rootless
+cp -a ${PWD}/include/ ${THEOS}/include/
